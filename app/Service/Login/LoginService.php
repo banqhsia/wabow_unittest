4bee8274258955c0d7786e2a0b2d1741de148378
dio <dio@wabow.com>
2018-11-07 19:53:28 +0800

    Create LoginServiceForTest

--- app/Service/Login/LoginService.php
+++ app/Service/Login/LoginService.php
@@ -8,7 +8,8 @@ class LoginService
 {
     public function login($email, $password)
     {
-        $user = new User;
+        $user = $this->getUser();
+        // $user = new User;
 
         if ($user->isExists($email) && $user->getPassword($email) === $password) {
             return "Login success";
@@ -16,4 +17,10 @@ class LoginService
 
         return "Not exists";
     }
+
+    protected function getUser()
+    {
+        return new User;
+    }
+
 }
--- tests/LoginServiceTest.php
+++ tests/LoginServiceTest.php
@@ -9,11 +9,34 @@ class LoginServiceTest extends TestCase
 {
     public function test_should_login_success()
     {
-        $target = new LoginService;
+        $target = new LoginServiceForTest;
 
         $expected = 'Login success';
-        $actual = $target->login('abc@example.com', md5('abc123'));
+        $actual = $target->login('abc@example.com', 'Happy');
 
         $this->assertEquals($expected, $actual);
     }
 }
+
+
+class FakeUser extends \App\User
+{
+    public function isExists($email)
+    {
+        return true;
+    }
+
+    public function getPassword()
+    {
+        return 'Happy';
+    }
+}
+
+class LoginServiceForTest extends LoginService
+{
+    protected function getUser()
+    {
+        return new FakeUser;
+    }
+}
+
