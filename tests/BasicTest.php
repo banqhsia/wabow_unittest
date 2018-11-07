682fbde7b0ac3dfae2d28ca30e4c18d181b12236
Ben <ben@wabow.com>
2018-11-07 15:31:00 +0800

    Composer init

---
+++ tests/BasicTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class BasicTest extends TestCase
+{
+    public function test_should_be_true()
+    {
+        $this->assertTrue(true);
+    }
+}
